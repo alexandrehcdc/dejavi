38cf4f790c8dafcebf4d7938e88b137d06acf186
dimitri frazao <dimitrilf@dimitri-frazaos-macbook-pro.local>
2017-07-01 20:04:57 -0300

    initial model structure and APICalls file

---
+++ dejavi/APICalls.swift
@@ -0,0 +1,9 @@
+//
+//  APICalls.swift
+//  dejavi
+//
+//  Created by dimitri frazao on 7/1/17.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+import Foundation
---
+++ dejavi/Movies.swift
@@ -0,0 +1,25 @@
+
+import UIKit
+import
+
+class MovieModel {
+    var title, year, poster, type, imdbId: String
+    
+    struct rating {
+        var source, value: String?
+    }
+    
+    struct movieDescription {
+        var rated, runtime, genre, plot, awards: String
+        var ratings : [rating] = []
+    }
+    
+    init(title: String, year: String, poster: String, type: String, imdbId: String) {
+        self.title = title
+        self.year = year
+        self.poster = poster
+        self.type = type
+        self.imdbId = imdbId
+    }
+    
+}
