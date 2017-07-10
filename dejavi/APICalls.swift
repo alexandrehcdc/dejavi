e82e73a93da03ac1d8a0f2f803340ed90510d98a
alexandrehcdc <ahc__@hotmail.com>
2017-07-10 03:32:06 -0300

    updated API call structure

--- dejavi/APICalls.swift
+++ dejavi/APICalls.swift
@@ -1,7 +1,31 @@
 
 import Foundation
+import Alamofire
 
 class APICalls {
-    var basePath : String = "http://www.omdbapi.com/?apikey=c22bc403"
+    var basePath : String = "http://www.omdbapi.com/?apikey=ec6483bd?"
+    var newModel = MovieModel()
     
+    func callAPI (paramsToBeSearched: String, _ typeOfOperation: String) -> NSData? {
+        guard !paramsToBeSearched.isEmpty && !typeOfOperation.isEmpty else {
+            return nil
+        }
+        let requestPath : String = "\(basePath)\(typeOfOperation)\(paramsToBeSearched)"
+        var teste : NSData?
+        Alamofire.request(.GET, requestPath)
+            .responseString { _, _, result in
+                print("Success: \(result.isSuccess)")
+                print("Response String: \(result.value)")
+                teste = result.data
+        }
+        return teste
+    }
+    
+    func getByTitle (titleToBeSearched: String) {
+        print(callAPI(titleToBeSearched, "?t="))
+    }
+    
+    func getBySearch (titleToBeSearched: String) {
+        print(callAPI(titleToBeSearched, "?s="))
+    }
 }
